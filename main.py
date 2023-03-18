33bedb9434d693d0aad934b5ff78937d2a8b208b
Марк <fenster.mark@yandex.ru>
2023-03-18 22:41:25 +0200

    Relations created

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import psycopg2 as pg
+import config
+database = config.database
+user = config.user
+password = config.password
 
-conn = pg.connect(database='clients', user='postgres', password='FemarData')
+conn = pg.connect(database=database, user=user, password=password)
 
 with conn.cursor() as cur:
-    cur.execute('CREATE TABLE IF NOT EXISTS name(id SERIAL INTEGER PRIMARY KEY, )')
+    cur.execute('''
+    DROP TABLE IF EXISTS phone;
+    DROP TABLE IF EXISTS client_name;
+    ''')
+
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS client_name(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(120) NOT NULL,
+                patronymic VARCHAR(120) NOT NULL,
+                sirname VARCHAR(120) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL);
+                ''')
+    cur.execute('''
+                CREATE TABLE IF NOT EXISTS phone(
+                id SERIAL PRIMARY KEY,
+                client_id INTEGER NOT NULL REFERENCES client_name(id),
+                phone_number INTEGER);
+                ''')
+    conn.commit()
 conn.close()
