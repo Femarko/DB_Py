6ac77d849cda60f9e649c79ec57b1c600c24f412
Марк <fenster.mark@yandex.ru>
2023-03-25 01:38:55 +0300

    4 functions

--- main.py
+++ main.py
@@ -31,11 +31,15 @@ def create_relations():
     conn.commit()
 
 # 2. Функция, позволяющая добавить нового клиента
-def insert_new_client(cursor, name, patronymic, sirname, email):
+def insert_new_client(cursor, name, patronymic, sirname, email, phone=None):
     cursor.execute('''
         INSERT INTO client_name (name, patronymic, sirname, email)
-        VALUES (%s, %s, %s, %s)
+        VALUES (%s, %s, %s, %s) RETURNING id
     ''', (name, patronymic, sirname, email))
+    # if phone is not None:
+    #     cursor.execute('''
+    #
+    #     ''')
     conn.commit()
 
 # 3. Функция, позволяющая добавить телефон для существующего клиента
@@ -48,30 +52,39 @@ def insert_phone(cursor, client_id, phone_number):
 
 # 4. Функция, позволяющая изменить данные о клиенте
 def client_data_update(cursor, id, name=None, patronymic=None, sirname=None, email=None):
-    column_s_list = [name, patronymic, sirname, email]
+    if name is not None:
+        cursor.execute('''
+            UPDATE client_name
+            SET name = %s
+            WHERE id=%s;''', (name, id))
+    if patronymic is not None:
+        cursor.execute('''
+            UPDATE client_name
+            SET patronymic = %s
+            WHERE id = %s;''', (patronymic, id))
+    if sirname is not None:
+        cursor.execute('''
+            UPDATE client_name
+            SET sirname = %s
+            WHERE id = %s;''', (sirname, id))
+    if email is not None:
+        cursor.execute('''
+            UPDATE client_name
+            SET email = %s
+            WHERE id = %s;''', (email, id))
+
+# 5. Функция, позволяющая удалить телефон для существующего клиента
+def delete_phone_number(cursor, id):
     cursor.execute('''
-        UPDATE Client
-        SET сюда можно будет указать любой параметр, например name, остальные останутся прежние.
-        WHERE client_id=client_id;''', (*column_s_list, id,))
+        SELECT id FROM client_name 
+    ''')
 
-
-
-def client_data_update(cursor, id, name=None, patronymic=None, sirname=None, email=None):
-    columns_dict = {'name': name, 'patronymic': patronymic, 'sirname': sirname, 'email': email}
-
-    for column_name, column_data in columns_dict.items():
-        if columns_dict[column_name] is not None:
-            cursor.execute('''
-                UPDATE Client 
-                SET %s
-                WHERE id=%(id)s;''', (*column_s_list, id,))
-        else:
-            print(column)
-
+    if id in select_result:
+        pass
 
 with conn.cursor() as cur:
-    insert_phone(cur, '1', '79169053323')
-
+    # client_data_update(cur, '1', name='Feokt')
+    print(insert_new_client(cur, '123', '123', '123', '124', '124'))
     conn.commit()
 
     # create_relations()
